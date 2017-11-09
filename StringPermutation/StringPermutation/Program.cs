ca03f67f184f3b3690a0fb99e5f716c8d7365b09
Gregory Dukes <kcils360@gmail.com>
2017-11-09 09:18:53 -0800

    commented out dynamic code, hard code working

--- StringPermutation/StringPermutation/Program.cs
+++ StringPermutation/StringPermutation/Program.cs
@@ -6,29 +6,28 @@ namespace StringPermutation
     {
         static void Main(string[] args)
         {
-            string[] arr = new string[] {}; //{"a", "b," "c", "d"};
+            string[] arr = new string[] {"a", "b", "c", "d"};
             int n = arr.Length-1;
 
             Permutation go = new Permutation();
 
-            //go.Permut(arr, 0, n);
-            //Console.ReadKey();
-            Console.WriteLine("Please type the NUMBER of letters to permutate");
-            int u = Convert.ToInt32( Console.ReadLine() );
-            //int i = 0;
-            string[] alphabet = new string[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
-
-            for(int i = 0; i < u; i++)
-            {
-                string val = alphabet.GetValue(i).ToString();
-                arr[i] = val;
-                i++;
-            }
-            foreach (string a in arr)
-            {
-                Console.WriteLine(a);
-            }
+            go.Permut(arr, 0, n);
             Console.ReadKey();
+            //    Console.WriteLine("Please type the NUMBER of letters to permutate");
+            //    int u = Convert.ToInt32( Console.ReadLine() );
+            //    string[] alphabet = new string[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+
+            //    for(int i = 0; i < u; i++)
+            //    {
+            //        string val = alphabet.GetValue(i).ToString();
+            //        arr[i] = val;
+            //        i++;
+            //    }
+            //    foreach (string a in arr)
+            //    {
+            //        Console.WriteLine(a);
+            //    }
+            //    Console.ReadKey();
         }
     }
 }
